ba90294c7501b03e727fc8e3e7ce146889adda3f
Arturo Rebolledo <elrayoarturopro@outlook.com>
2021-06-18 10:23:49 -0500

    Added Eslint Prettier and Husky

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
@@ -17,15 +17,15 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.(s*)css$/,
         use: [
           { loader: MiniCssExtractPlugin.loader },
-          "css-loader",
-          "sass-loader",
+          'css-loader',
+          'sass-loader',
         ],
       },
     ],
@@ -33,17 +33,18 @@ module.exports = {
   // @ts-ignore
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
+      template: './public/index.html',
+      filename: './index.html',
+      inject: 'body',
     }),
     new MiniCssExtractPlugin({
-      filename: "assets/[name].css",
+      filename: 'assets/[name].css',
     }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3010,
-    host: "172.19.127.96",
+    // host: "172.19.127.96",
   },
 };
