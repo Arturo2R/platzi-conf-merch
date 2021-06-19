0ff4c62b9562466e3c999a081dd942abba636888
Arturo Rebolledo <elrayoarturopro@outlook.com>
2021-06-19 17:31:12 -0500

    Creado lás pagina de Checkout e Información

--- src/views/Information.jsx
+++ src/views/Information.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import '../styles/components/Information.css';
 
 const Information = () => {
   return (
-    <div>
-      <h1>Information</h1>
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Informatiion-heade">
+          <h2>Información de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="NombreCompleto" name="name" />
+            <input type="text" placeholder="Correo Electrónico" name="email" />
+            <input type="text" placeholder="Dirección" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="País" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Código Postal" name="cp" />
+            <input type="text" placeholder="Telefono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">Pagar</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>
+          Pedido:
+          <div className="Information-item">
+            <div className="Information-Element">
+              <h4>Item Name</h4>
+              <span>$10</span>
+            </div>
+          </div>
+        </h3>
+      </div>
     </div>
   );
 };
